480fedee1fd23ac111d31d21928e51fdffe88114
Khalid Sookia <khalidsookia@gmail.com>
2016-12-08 13:42:56 +0100

    added created at helper method

---
+++ CreatedAtTrait.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by iKNSA.
+ * Author: Khalid Sookia <khalidsookia@gmail.com>
+ * Date: 28/11/16
+ * Time: 19:50
+ */
+
+namespace Romenys\Helpers;
+
+trait CreatedAtTrait
+{
+    public function setCreatedAt($dateParams = null)
+    {
+        $dateParams = is_null($dateParams) ? "NOW" : $dateParams;
+        $this->createdAt = new \DateTime($dateParams);
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        $this->setCreatedAt();
+
+        return $this->getCreatedAt();
+    }
+}
+
